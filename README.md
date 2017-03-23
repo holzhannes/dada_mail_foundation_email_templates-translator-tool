f9849ce97b64ccd36583865f4c17fe95626e01cf
holzhannes <johannes@joPro.fritz.box>
2017-03-23 11:09:16 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dada_mail_foundation_email_templates-translator-tool
 Tool to translate the dada_mail_foundation_email_templates from a .csv file through a python skript
+
+
+# How to use?
+
+Place the unziped folder of [dada\_mail\_foundation\_email\_templates](https://github.com/justingit/dada_mail_foundation_email_templates/) on the same level with the `dada-mail-translation-de-formal.csv` (german translation .csv you may create your own) together with the `translate.py` and run the comand: `pyhton translate.py`.
+
+# Note
+
+This is just a simple search and replace tool in plaintext multiline replacing is working. In HTML-Files multiline replace including insertions is NOT working, therefore these textparts are splited in single lines.
---
+++ translate.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# python 2
+# script for multiple find/replace pairs for all HTML/TXT files in a directory.
+
+import os,sys,csv,re
+
+inputDir = "dada_mail_foundation_email_templates-master"
+translateCSV = "dada-mail-translation-de-formal.csv"
+
+#filename;original,translation
+data = csv.reader(open(translateCSV,'rU'),delimiter=';')
+
+findReplace = []
+
+for items in data:
+    findReplace.append(items)
+
+#sort the list, to first replace long strings
+findReplace.sort(key=lambda t: len(t[1]), reverse=True)
+
+for item in findReplace:
+  print item[1]
+
+def replaceStringInFile(filePath):
+   "replaces all findStr by repStr in file filePath"
+   tempName = filePath+'~~~'
+
+   inputFile = open(filePath)
+   outputFile = open(tempName, 'w')
+   fContent = unicode(inputFile.read(), "utf-8")
+
+   for aPair in findReplace:
+    if len(aPair[2].decode("utf-8")) > 0: #ignore empty translations
+      outputText = re.sub(re.escape(aPair[1].decode("utf-8")), aPair[2].decode("utf-8"), fContent, count=0, flags=0)
+      fContent = outputText
+
+   outputFile.write(outputText.encode("utf-8"))
+
+   outputFile.close()
+   inputFile.close()
+
+   print "translated {}".format(filePath)
+   os.rename(tempName, filePath)
+
+def fileFilter(dummyArg, thisDir, dirChildrenList):
+    for thisChild in dirChildrenList:
+        if '.html' == os.path.splitext(thisChild)[1] and os.path.isfile(thisDir+'/'+thisChild):
+            replaceStringInFile(thisDir+'/'+thisChild)
+        if '.txt' == os.path.splitext(thisChild)[1] and os.path.isfile(thisDir+'/'+thisChild):
+            replaceStringInFile(thisDir+'/'+thisChild)
+
+os.path.walk(inputDir, fileFilter, None)
